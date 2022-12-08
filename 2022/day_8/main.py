62b46d2648b3d3a284e9cf4a47a05abdaba2c2bb
thiemo seys <thiemo.seys@nipro-group.com>
2022-12-08 13:42:18 +0100

    not exactly the definition of clean code

---
+++ 2022/day_8/main.py
@@ -0,0 +1,119 @@
+from dataclasses import dataclass
+
+
+def input_data(path: str):
+    with open(path) as f:
+        return f.read().splitlines()
+
+
+@dataclass
+class Grid:
+    board: list
+    width: int
+    height: int
+
+    def __init__(self, board, width, height):
+        self.board = [int(item) for sublist in board for item in sublist]
+        self.width = width
+        self.height = height
+
+    def get_row(self, index):
+        return self.board[index * self.width: index * self.width + self.width]
+
+    def get_column(self, index):
+        return [self.board[i * self.height + index] for i in range(self.height)]
+
+    def value_by_index(self, x, y):
+        val_index = y * self.height + x
+        return self.board[val_index]
+
+    def is_visible(self, x, y):
+        tree_val = self.value_by_index(x, y)
+
+        row = self.get_row(y)
+        column = self.get_column(x)
+
+        left = row[:x]
+        right = row[x + 1:]
+        up = column[:y]
+        down = column[y + 1:]
+
+        for arr in [left, right, up, down]:
+            if max(arr) < tree_val:
+                return True
+
+        return False
+
+    def score_tree(self, x, y):
+        tree_val = self.value_by_index(x, y)
+
+        # slice left, right, up and down
+        row = self.get_row(y)
+        column = self.get_column(x)
+
+        left = row[:x]
+        right = row[x + 1:]
+        up = column[:y]
+        down = column[y + 1:]
+
+        score = 1
+        for direction in [reversed(left), right, reversed(up), down]:
+            view_distance = self.calculate_view_distance(tree_val, direction)
+            score *= view_distance
+        return score
+
+    def calculate_view_distance(self, height, neighbours):
+        counter = 0
+        for tree in neighbours:
+            if tree < height:
+                counter += 1
+            else:
+                counter += 1
+                break
+
+        return counter
+
+    def visible_trees(self):
+        counter = 0
+        for x in range(self.width):
+            for y in range(self.height):
+                # trees on edges
+                if x == 0 or x == self.width - 1 or y == 0 or y == self.height - 1:
+                    counter += 1
+                    continue
+
+                if self.is_visible(x, y):
+                    counter += 1
+                    # print(f"center tree: {x, y} is visible with value: {self.value_by_index(x, y)}")
+
+        return counter
+
+    def most_scenic_tree(self):
+        tree_scores = []
+        for x in range(self.width):
+            for y in range(self.height):
+                if x == 0 or x == self.width - 1 or y == 0 or y == self.height - 1:
+                    tree_scores.append(0)
+                    continue
+                tree_scores.append(self.score_tree(x, y))
+        return tree_scores
+
+    def pretty_print(self):
+        print(f"{self.width=}")
+        print(f"{self.height=}")
+        for i in range(self.height):
+            print(self.get_row(i))
+
+
+if __name__ == "__main__":
+    data = input_data("data/input.txt")
+    grid = Grid(board=data, width=len(data[0]), height=len(data))
+    grid.pretty_print()
+    print(f"*" * 120)
+
+    # part 1
+    print(grid.visible_trees())
+
+    # part 2
+    scores = grid.most_scenic_tree()
+    print(f"top tree: {max(scores)}")
